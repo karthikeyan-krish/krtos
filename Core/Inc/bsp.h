befeca46034828cae45db7446d06d67212c9e532
karthikeyan-krish <imkarthiknk@gmail.com>
2025-05-22 18:13:09 +0200

    DEV[MAJ]: add systick handler and delay functions

--- Core/Inc/bsp.h
+++ Core/Inc/bsp.h
@@ -1,6 +1,15 @@
 #ifndef __BSP_H__
 #define __BSP_H__
 
+// system clock tick [Hz]
+#define BSP_TICKS_PER_SEC 100U
+
+// get the current value of the clock tick counter (returns immedately)
+uint32_t BSP_tickCtr(void);
+
+// delay for a specified number of system clock ticks (polling)
+void BSP_delay(uint32_t ticks);
+
 // initialize gpio for LED interface
 void BSP_init(void);
 
--- Core/Src/bsp.c
+++ Core/Src/bsp.c
@@ -7,6 +7,28 @@
 #define LED_GREEN 14
 #define LED_BLUE 9
 
+static uint32_t volatile l_tickCtr;
+
+void SysTick_Handler(void) {
+    ++l_tickCtr;
+}
+
+uint32_t BSP_tickCtr(void) {
+    uint32_t tickCtr;
+
+    __disable_irq();
+    tickCtr = l_tickCtr;
+    __enable_irq();
+
+    return tickCtr;
+}
+
+void BSP_delay(uint32_t ticks) {
+    uint32_t start = BSP_tickCtr();
+    while ((BSP_tickCtr() - start) < ticks) {
+    }
+}
+
 void BSP_init(void) {
     RCC->AHB2ENR  |= ((1U << 1) | (1U << 2));   // Enable clock for gpiob (bit 1) and gpioc (bit 2)
 
