1a12976d474bb3f25e83b5ca709dcb13587da9d2
karthikeyan-krish <imkarthiknk@gmail.com>
2025-06-12 10:13:23 +0200

    DEV[MAJ]: create threads for context switching

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -54,6 +54,23 @@ static void MX_GPIO_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void main_blinky1(){
+  while(1){
+    BSP_ledGreenOn();
+    BSP_delay(BSP_TICKS_PER_SEC / 4U);
+    BSP_ledGreenOff();
+    BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+  }
+}
+
+void main_blinky2(){
+  while(1){
+    BSP_ledBlueOn();
+    BSP_delay(BSP_TICKS_PER_SEC / 4U);
+    BSP_ledBlueOff();
+    BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+  }
+}
 
 /* USER CODE END 0 */
 
