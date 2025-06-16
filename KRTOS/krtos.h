e4e5b97c38534dd97676c894fed4cd20dff1f5ef
karthikeyan-krish <imkarthiknk@gmail.com>
2025-06-16 11:39:27 +0200

    DEV[MAJ]: create OS thread pointer and thread handler prototype

--- KRTOS/krtos.h
+++ KRTOS/krtos.h
@@ -1,4 +1,11 @@
 #ifndef KRTOS_H
 #define KRTOS_H
 
+/* thread control block (TCB) */
+typedef struct {
+    void *sp; /* stack sointer */
+} OSThread;
+
+typedef void (*OSThreadHandler)();
+
 #endif /*KRTOS_H*/
